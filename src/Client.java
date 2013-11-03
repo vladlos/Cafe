da08d01590eeb5e61a13991f26e4058fade47013
vladlos <vlad_los@mail.ru>
2013-11-03 14:58:55 +0200

    now client has field "meal" and can go to the casier and fill it

--- src/Client.java
+++ src/Client.java
@@ -5,8 +5,17 @@
  */
 public class Client extends Thread{
     private String clientId;
+    private Cassier cassier;
+    private Meal meal;
 
-    public Client(String clientId) {
+    public Client(String clientId, Cassier cassier) {
         this.clientId = clientId;
+        this.cassier = cassier;
+    }
+
+    @Override
+    public void run() {
+        meal = cassier.giveMeal();
+        System.out.println("client #"+clientId+" buy meal #"+meal.getId());
     }
 }
