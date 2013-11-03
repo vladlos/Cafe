469a83ebbc9d2e0495b8c90ffd688c6aa8c4f524
vladlos <vlad_los@mail.ru>
2013-11-03 14:42:23 +0200

    added id to meal

--- src/Meal.java
+++ src/Meal.java
@@ -1,9 +1,15 @@
 /**
- * Created with IntelliJ IDEA.
  * User: Vlad
  * Date: 03.11.13
  * Time: 14:19
- * To change this template use File | Settings | File Templates.
  */
 public class Meal {
+    private String id;
+    public Meal(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
