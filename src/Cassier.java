f31f86a08ee81933770c8f7ae63b6d2805178e22
vladlos <vlad_los@mail.ru>
2013-11-03 15:12:28 +0200

    added working time for give food

--- src/Cassier.java
+++ src/Cassier.java
@@ -15,7 +15,12 @@ public class Cassier{
     }
 
     public Meal giveMeal(){
-     if (meals.size()>0){
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        if (meals.size()>0){
          return meals.remove(0);
         }
      else {
