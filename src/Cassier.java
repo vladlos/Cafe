862f9e5dbd7dde66e1f925a1ca21588e15fb211e
vladlos <vlad_los@mail.ru>
2013-11-03 14:24:12 +0200

    first structure

---
+++ src/Cassier.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vlad
+ * Date: 03.11.13
+ * Time: 14:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class Cassier implements Runnable {
+    @Override
+    public void run() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
---
+++ src/Client.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vlad
+ * Date: 03.11.13
+ * Time: 14:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class Client extends Thread{
+}
