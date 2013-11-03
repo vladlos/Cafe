2e2f754e508153e2022ef9e1ee5062101962663f
vladlos <vlad_los@mail.ru>
2013-11-03 14:47:29 +0200

    added id and constructor

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,12 @@
 /**
- * Created with IntelliJ IDEA.
  * User: Vlad
  * Date: 03.11.13
  * Time: 14:19
- * To change this template use File | Settings | File Templates.
  */
 public class Client extends Thread{
+    private String clientId;
+
+    public Client(String clientId) {
+        this.clientId = clientId;
+    }
 }
